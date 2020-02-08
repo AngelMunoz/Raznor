c557a4c3f2a4e76badb69e699433fcb0802aeba0
Angel D. Munoz <scythes.lol@gmail.com>
2020-02-08 00:21:28 -0700

    small script to build a single exe

---
+++ build.ps1
@@ -0,0 +1,4 @@
+function New-Build {
+  Param([string] $rid = 'win10-x64')
+  dotnet publish Raznor -c Release -r $rid --self-contained true -p:PublishTrimmed=true -p:PublishSingleFile=true -o dist
+}
