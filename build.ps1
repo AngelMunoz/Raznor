575ca6f35514b2172187f163ca0c45065b72ec00
Angel D. Munoz <scythes.lol@gmail.com>
2020-02-09 09:00:59 -0700

    update script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-function New-Build {
-  Param([string] $rid = 'win10-x64')
-  dotnet publish Raznor -c Release -r $rid --self-contained true -p:PublishTrimmed=true -p:PublishSingleFile=true -o dist
-}
+Param([string] $rid = 'win10-x64')
+dotnet publish Raznor -c Release -r $rid --self-contained true -p:PublishTrimmed=true -p:PublishSingleFile=true -o dist
+
+
