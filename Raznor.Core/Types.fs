51ba58a757e5d990da069e6f181974356909b43b
Angel D. Munoz <scythes.lol@gmail.com>
2022-03-21 07:17:07 -0600

    add new types

--- Raznor.Core/Types.fs
+++ Raznor.Core/Types.fs
@@ -1,15 +1,39 @@
 namespace Raznor.Core
 
 module Types =
-  open System
+    open System
 
-  type SongRecord =
-    { id : Guid
-      name : string
-      path : string
-      createdAt : DateTime }
+    type SongRecord =
+        { id: Guid
+          name: string
+          path: string
+          createdAt: DateTime }
 
-  type LoopState =
-    | Off
-    | All
-    | Single
+    type LoopState =
+        | Off
+        | All
+        | Single
+
+    type PlayDirection =
+        | Next
+        | Previous
+        | Direct of SongRecord
+
+    type PlayState =
+        | Play
+        | Pause
+        | Stop
+
+    type PlayerState =
+        { length: int64
+          song: SongRecord option
+          sliderPos: int
+          state: PlayState
+          loopState: LoopState }
+
+        static member Empty =
+            { length = 0
+              song = None
+              sliderPos = 0
+              state = PlayState.Stop
+              loopState = LoopState.All }
